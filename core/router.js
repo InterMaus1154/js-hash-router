c72c14c04e265038e2d2997c1071746ae65f89af
InterMaus1154 <amarco1154@gmail.com>
2024-10-22 12:07:02 +0100

    basic route registry constrains

--- core/router.js
+++ core/router.js
@@ -1,6 +1,11 @@
 
 
 export default class Router {
+
+    #routes = {};
+
+    #routePattern = /[^a-zA-Z0-9/#]/;
+
     constructor() {
         window.addEventListener("hashchange", e => {
 
@@ -9,9 +14,38 @@ export default class Router {
 
     /**
      * Register new routes in the router
+     * @param route - route excluding # at the beginning
+     * @param view - relative path to project root for the corresponding view
+     * @param index - false by default, set to true if page is default (/) page aka home
      */
-    register(route, view){
+    register(route, view, index = false){
+        route = route.replaceAll("#", "");
 
+        if(route.trim().length === 0 || view.trim().length === 0){
+            console.error("Route or view is empty!");
+            console.error("Route:" + route);
+            console.error("View: "+ view);
+            console.error("Route and view must have non-empty values!");
+        }
+
+        if(this.#routes[route]){
+            console.error("This route already is already registered!");
+            console.error(route);
+            console.error("All routes must be unique!");
+            return;
+        }
+
+        if(this.#routePattern.test(route)){
+            console.error("Route contains illegal characters!");
+            console.error(route);
+            console.error("Only letters, numbers, / and # signs are allowed!");
+            return;
+        }
+
+        this.#routes[route] = {
+            view,
+            index
+        };
     }
 
 
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import Router from "./core/router.js";
 
 const router = new Router();
+router.register("home", "view");
+router.register("home", "view");
